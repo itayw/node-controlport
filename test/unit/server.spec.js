ff74ca5e33805d418a99fc4f78f23b60f559fb9e
Itay Weinberger <itay@joo.la>
2013-09-19 17:02:51 +0300

    Updated tests

--- test/unit/server.spec.js
+++ test/unit/server.spec.js
@@ -20,16 +20,43 @@ describe("server", function () {
     });
   });
 
-  it("should run the HTTP server", function () {
-    http.request('http://localhost:42111/status', function (data) {
-      expect(data).to.be.ok;
+  it("should run the HTTP server", function (done) {
+    var options = {
+      hostname: '127.0.0.1',
+      port: 42111,
+      path: '/',
+      method: 'GET'
+    };
+
+    var request = require('supertest'),
+    request = request('http://localhost:42111');
+    request.get('/').expect(404, function (err) {
+      if (err)
+        return done(err);
+      done();
     });
   });
 
-  it("should have a valid endpoint", function () {
-    http.request('http://localhost:42111/status', function (data) {
-      expect(data).to.be.ok;
-    })
+  it("should have a valid endpoint", function (done) {
+    var options = {
+      hostname: 'localhost',
+      port: 42111,
+      path: '/status',
+      method: 'GET'
+    };
+
+    var request = require('supertest'),
+    request = request('http://localhost:42111/status');
+    request.get('/').expect(200).end(function (err, res) {
+      if (err)
+        return done(err)
+
+      var result = res.body.status;
+      expect(result).to.equal('Success');
+
+      done();
+
+    });
   });
 
   after(function (done) {
